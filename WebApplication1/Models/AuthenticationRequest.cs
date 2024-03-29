6c20032d376b7fb6cd3471a770a83631c099e15a
Toxa70ru <42457726+Toxa70ru@users.noreply.github.com>
2024-03-29 15:51:16 +0700

    Delete WebApplication1 directory

--- WebApplication1/Models/AuthenticationRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class AuthenticationRequest
-    {
-        public string Username { get; set;}
-        public string Password { get; set;}
-    }
-}
